186cdb2603391e071958d137f6a5839dd75be268
thomaschiari <thomaschiari@gmail.com>
2024-02-28 08:17:02 -0300

    create parser + updates

--- src/main/java/insper/store/account/AccountService.java
+++ src/main/java/insper/store/account/AccountService.java
@@ -1,12 +1,39 @@
 package insper.store.account;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
-    public AccountModel create(Account in) {
-        return accountRepository.save(new AccountModel(in).to());
+    public Account create(Account in) {
+        in.hash(calculateHash(in.password()));
+        in.password(null);
+        return accountRepository.save(new AccountModel(in)).to();
+    }
+
+    public Account read(String id) {
+        return accountRepository.findById(id).map(AccountModel::to).orElse(null);
+    }
+
+    public Account login(String email, String password) {
+        String hash = calculateHash(password);
+        return accountRepository.findByEmailAndHash(email, hash).map(AccountModel::to).orElse(null);
+    }
+
+    private String calculateHash(String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+            byte[] encoded = Base64.getEncoder().encode(hash);
+            return new String(encoded);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
